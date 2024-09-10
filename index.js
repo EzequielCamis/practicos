b486ab4eecb165c193632e250e6f640561b7c8f3
ezequiel <ezequiel.camisassa.123@gmail.com>
2024-09-10 20:35:25 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const prompt = require('prompt-sync')();
+
+const nombre = prompt('¿Cuál es tu nombre?');
+console.log(`¡Hola ${nombre}!`);
+console.log("Qué deseas hacer");
+console.log("[1] Ver Mis Tareas.");
+console.log("[2] Buscar una Tarea.");
+console.log("[3] Agregar una Tarea");
+console.log("[0] Salir");
+
